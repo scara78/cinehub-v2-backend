e5d2209611309384188669e660637f5b93404ae2
scara78 <61316528+scara78@users.noreply.github.com>
2023-02-25 10:04:53 +0200

    Update getStreamLinks.js

--- routes/all/getStreamLinks.js
+++ routes/all/getStreamLinks.js
@@ -2,7 +2,7 @@ const getStreamLinks = async (fastify) => {
   fastify.get("/api/watch", async (request, reply) => {
     let { id, epid } = request.query;
     const req = await fetch(
-      `https://api.consumet.org/movies/flixhq/watch?mediaId=${id}&episodeId=${epid}`
+      `https://c.delusionz.xyz/movies/flixhq/watch?mediaId=${id}&episodeId=${epid}`
     );
     const res = await req.json();
     return { result: res };
